88658bdab77cafc467dfaf3b0a094560a00d6576
Yassin Youssoufm <yassinrsc@gmail.com>
2022-12-06 13:14:36 +0300

    Add initial "direct order" page (WIP)

---
+++ pages/order/direct.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container mx-auto mt-8">
+    <section>
+      <div class="flex flex-col gap-x-14 lg:flex-row">
+        <div class="lg:w-1/2">
+          <p>We specialize in group orders for offices, parties, & events!</p>
+          <p>
+            Our stuffed breakfast sandwiches will delight your coworkers or
+            guests with light, airy dough wrapped around delicious fillings!
+          </p>
+          <p>We’ll guide you through the process to make the perfect order.</p>
+          <nuxt-link to="/" class="btn btn-primary mt-10 hidden uppercase lg:inline-block">Begin order</nuxt-link>
+
+        </div>
+        <div>
+            <img src="~/assets/img/kolaches.png" />
+        </div>
+      </div>
+      <nuxt-link to="/" class="btn btn-primary mt-8 w-full block uppercase lg:hidden">Begin order</nuxt-link>
+    </section>
+  </main>
+</template>
