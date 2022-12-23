56514c1e94a178bdbda10dd2f2695310dffe4bb3
Yassin Youssoufm <yassinrsc@gmail.com>
2022-12-23 12:06:49 +0300

    Implement successful order page layout

---
+++ pages/catering-success.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="container mx-auto mt-16">
+    <p class="text-center">
+      <span class="font-bold text-2xl">ORDER CONFIRMED!</span>
+      <span class="block text-5xl mt-10">🥳</span>
+    </p>
+    <div class="mt-16 lg:px-24">
+      <p>Woohoooo! Your order for 9am on Dec 20 is confirmed!</p>
+
+      <p>We’ll be sending you an email confirmation shortly.</p>
+
+      <p>
+        If you have any questions or need to make adjustments, please email us
+        directly at howdy@howdybreakfastbuns.com
+      </p>
+    </div>
+  </main>
+</template>
