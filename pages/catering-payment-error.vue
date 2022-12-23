09ae42423411aba7c4daae808766a50ce09a6d71
Yassin Youssoufm <yassinrsc@gmail.com>
2022-12-23 12:22:43 +0300

    Implement payment error page layout

---
+++ pages/catering-payment-error.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="container mx-auto mt-16">
+    <p class="text-center">
+      <span class="font-bold text-2xl">Uh oh...</span>
+      <span class="block text-5xl mt-10">😭</span>
+    </p>
+    <div class="mt-16 lg:px-24">
+      <p>
+        Unfortunately we seem to have had a problem processing your credit card.
+      </p>
+
+      <p>
+        If that’s an error you can press back on your browser a few times to
+        retry.
+      </p>
+
+      <p>
+        Or if you need help you can email us at howdy@howdybreakfastbuns.com
+        directly and we’ll handle your order directly for you.
+      </p>
+    </div>
+  </main>
+</template>
