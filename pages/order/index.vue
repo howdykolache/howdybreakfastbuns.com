fba1d6888baaddd207dd478fb7db3c9d664158da
ezl <ericzliu@gmail.com>
2022-12-30 11:33:16 -0600

    update order landing page

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -9,6 +9,25 @@
           Order breakfast buns weekdays 7:30am-1:30pm via Toast, ezCater, or
           through us directly.
         </p>
+        <section
+          class="links mt-10 grid grid-cols-2 grid-rows-2 gap-4"
+        >
+          <nuxt-link class="order-button directly" to="/order/direct">Order Directly</nuxt-link>
+          <a
+            class="order-button toasttab"
+            href="https://www.toasttab.com/howdybreakfastbuns/v3"
+            target="_blank"
+          >
+            <img src="@/static/toasttab.png" class="max-h-6" />
+          </a>
+          <a
+            class="order-button ezcater"
+            href="https://www.ezcater.com/catering/howdy-breakfast-buns-chicago"
+            target="_blank"
+          >
+            <img src="@/static/ezcater.png" class="max-h-6" />
+          </a>
+        </section>
         <p class="mt-5 mb-1 lg:mt-12">
           Please
           <a
@@ -26,18 +45,30 @@
         <img src="~/assets/img/howdy2.png" alt="Howdy Breakfast Buns" />
       </div>
     </section>
-    <section
-      class="links mt-10 flex flex-col items-center gap-y-6 lg:mt-20 lg:flex-row lg:gap-y-0 lg:gap-x-12"
-    >
-      <a href="https://www.toasttab.com/howdybreakfastbuns/v3">TOAST</a>
-      <a href="https://www.ezcater.com/catering/howdy-breakfast-buns-chicago">ezCater</a>
-      <nuxt-link to="/order/direct">Through us directly</nuxt-link>
-    </section>
+
+
   </main>
 </template>
 
 <style>
 .links a {
-  @apply bg-primary w-full lg:w-1/3 p-3 text-center font-bold text-lg lg:p-4 lg:text-xl;
 }
+
+.order-button {
+  @apply border text-center py-4;
+  foo: 'px-12 py-4 m-4 w-full lg:w-1/3 p-3';
+}
+.order-button > img {
+  @apply inline-block;
+}
+.order-button.directly {
+  @apply bg-primary w-full p-3 text-center text-lg lg:p-4 lg:text-xl col-span-2 font-bold hover:opacity-75;
+}
+.order-button.toasttab {
+  @apply border-orange-600  hover:bg-orange-50;
+}
+.order-button.ezcater {
+  @apply border-emerald-500  hover:bg-emerald-50;
+}
+
 </style>
